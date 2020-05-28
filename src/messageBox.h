d6d1e7b6e11b16a2572d84a27664fb4c673abbde
Clodio Pontes <danieljo2014@outlook.com>
2020-05-29 00:03:32 +0100

    Delete messageBox.h

--- src/messageBox.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-#include <string>
-#include <QMessageBox>
-#include <qdebug.h>
-
-// just a styled messageBox
-int styledQuestionMessageBox(std::string title, std::string description, std::string styleSheets, QMessageBox::StandardButtons buttons);
